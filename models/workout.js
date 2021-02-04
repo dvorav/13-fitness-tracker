aab5329baffb3b4ebb994aa932f79c961a6c56d7
dvorav <d.vorav612@gmail.com>
2021-02-03 21:23:32 -0600

    update workout.js

---
+++ models/workout.js
@@ -0,0 +1,46 @@
+//Require
+
+const mon = require("mongoose");
+const Schema = mon.Schema;
+
+//New Schema
+//Corelate to seeder.js
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: Number,
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      reps: {
+          type: Number,
+          default: 0
+      },
+      sets: {
+          type: Number, 
+          default: 0
+      },
+      distance: {
+          type: Number, 
+          default: 0
+      }
+    }
+  ],
+totalDuration: {
+    type: Number,
+    default: 0,
+}
+});
