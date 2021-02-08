ba7673c0eb5064b732d656ccbd8a07f5ded79afd
dvorav <d.vorav612@gmail.com>
2021-02-07 21:26:04 -0600

    heroku fix?

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -19,12 +19,37 @@ module.exports = (app) => {
         });
   });
 
-//   //Create a workout route
-//   app.post("/api/workouts", ({ body }, res) => {
-//     db.Workout.create(body).then((dbWorkout) => {
-//       res.json(dbWorkout);
-//     });
-//   });
+  //Create a workout route
+  app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then((dbWorkout) => {
+      res.json(dbWorkout);
+    });
+  });
+      // get workouts in range
+    app.get("/api/workouts/range", (req, res) => {
+
+        db.Workout.find({}).then(dbWorkout => {
+            console.log("ALL WORKOUTS");
+            console.log(dbWorkout);
+
+            res.json(dbWorkout);
+        })
+
+    });
+ // add exercise
+ app.put("/api/workouts/:id", (req, res) => {
+
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+
+});
 
 
 };
