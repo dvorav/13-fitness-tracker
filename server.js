a2fdce78d6a6728c46991cab6bd2bc89ff1eed86
dvorav <d.vorav612@gmail.com>
2021-02-07 21:35:27 -0600

    Dumb app

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/api-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 //HTML ROUTES 
 
 //Main page
