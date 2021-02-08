005a9d12066db135063207e43d7182e7ec4e4191
dvorav <d.vorav612@gmail.com>
2021-02-07 20:40:00 -0600

    Created a post route to create workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,30 +4,43 @@ module.exports = (app) => {
 
 
 //Get exercise routes
-
 app.get("/api/workouts", (req,res) => {
     //Grabs all workout with find({})
-    db.Workout.find({}).then(workoutdb => {
-workoutdb.forEach(workout => {
+    db.Workout.find({}).then(dbWorkout => {
+dbWorkout.forEach(workout => {
     let zero = 0;
     workout.exercises.forEach(e => {
         zero += e.duration;
     })
     workout.totalDuration = zero
 })
-res.json(workoutdb);
-});
+res.json(dbWorkout);
 })
 
+})
+
+//Create a workout route
+app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    }))
+   
+});
 
 
 
 
+app.put("/api/workouts/:id", (req, res) => {
 
-
-
-
-
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }) });
 
 
 
