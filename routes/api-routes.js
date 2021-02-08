104c8a36ae8f9c5e439719d5a32a53cddf7a77aa
dvorav <d.vorav612@gmail.com>
2021-02-07 20:11:38 -0600

    Get route workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,50 @@
 const db = require("../models")
+
+module.exports = (app) => {
+
+
+//Get exercise routes
+
+app.get("/api/workouts", (req,res) => {
+    //Grabs all workout with find({})
+    db.Workout.find({}).then(workoutdb => {
+workoutdb.forEach(workout => {
+    let zero = 0;
+    workout.exercises.forEach(e => {
+        zero += e.duration;
+    })
+    workout.totalDuration = zero
+})
+res.json(workoutdb);
+});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
